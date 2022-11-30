32385413ab5111ade56f567661d2056a2925679e
jonnahfreire <jonnas.freire17@gmail.com>
2022-11-30 11:39:23 -0300

    Refact views / routes

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const express = require('express')
-const { engine } = require('express-handlebars')
+const express = require('express');
+const conn = require("./services/index");
+const { engine } = require('express-handlebars');
 
-const cors = require('cors')
-const morgan = require('morgan')
-const bodyParser = require('body-parser')
+const cors = require('cors');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes')
-const app = express()
+const routes = require('./routes');
+const app = express();
 
 app.engine('handlebars', engine());
-app.set('view engine', 'handlebars')
-app.set('views', './views')
+app.set('view engine', 'handlebars');
+app.set('views', './views');
 
-app.use('/public', express.static(__dirname + '/public'))
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(express.json())
-app.use(cors())
-app.use(routes)
+app.use('/public', express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Express started at http://localhost:${port}`)
-})
+if(conn) {
+    console.log(`MySql Connected`);
+    
+    app.listen(port, () => {
+        console.log(`Express started at http://localhost:${port}`)
+    });
+
+} else {
+    console.log(`Houve um erro ao iniciar o serviço.`)
+}
